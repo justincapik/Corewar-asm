155f934254ea430cfb1f5c91bd3b3daf0ee1f5a8
Justin CAPIK <jucapik@e3r5p3.42.fr>
2019-04-27 16:26:11 +0200

    error tests

--- everything.sh
+++ everything.sh
@@ -1,5 +1,7 @@
 find . -name "*.cor" -exec rm {} \;
-find . -name "*.s" -exec ./asm {} \;
+rm error_file
+touch error_file
+find . -name "*.s" -exec valgrind ./asm {} \; 2>> error_file
 rm my_res
 touch my_res
 find . -name "*.cor" -exec hexdump {} \; >> my_res
@@ -8,4 +10,5 @@ find . -name "*.s" -exec ./asm_og {} \;
 rm og_res
 touch og_res
 find . -name "*.cor" -exec hexdump {} \; >> og_res
+cat error_file | grep "blocks" | grep "e" | grep "d" | grep "y" | grep -v "supp"
 diff my_res og_res
